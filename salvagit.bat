d99eadda25a33d44bc094a27152d54dcdb187e52
jaiderx <jaiderxavierjunior@gmail.com>
2019-09-08 02:39:35 -0400

    Aula 06 - Validacao de formularios - Ativ 01 - Validando os dados do formulario

--- salvagit.bat
+++ salvagit.bat
@@ -1,7 +1,7 @@
-﻿@echo off
+@echo off
 color 0a
 title Salva GIT
 cd %userprofile%\Downloads\introducao-javascript-inicial\introducao-javascript
 git add -A
-git commit -m "Aula 06 - Validação de formulários - Ativ 01 - Validando os dados do formulário"
+git commit -m "Aula 06 - Validacao de formularios - Ativ 01 - Validando os dados do formulario"
 git push -u -f origin master
