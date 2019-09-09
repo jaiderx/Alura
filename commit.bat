bfa2c850e37859f0cf81b96b7585e13a2db81d1b
jaiderx <jaiderxavierjunior@gmail.com>
2019-09-09 03:41:50 -0400

    Aula 06 - Validacao de formularios - Ativ 03 - Validando o restante dos campos

--- commit.bat
+++ commit.bat
@@ -1,6 +1,6 @@
 @echo off
 color 0a
-title Salva GIT
+title Commit GIT
 cd %userprofile%\Downloads\introducao-javascript-inicial\introducao-javascript
 git add -A
 git commit -m "Aula 06 - Validacao de formularios - Ativ 03 - Validando o restante dos campos"
