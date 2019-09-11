086ee8115ea2e149d7a6a8a783240885865fc159
jaiderx <jaiderxavierjunior@gmail.com>
2019-09-11 15:28:09 -0400

    Aula 08 - Filtrando uma tabela - Filtrando com expressao regular

--- commit.bat
+++ commit.bat
@@ -3,5 +3,5 @@ color 0f
 title Commit GIT
 cd %userprofile%\Downloads\introducao-javascript-inicial\introducao-javascript
 git add -A
-git commit -m "Aula 08 - Filtrando uma tabela - Implementando a lógica de filtragem"
+git commit -m "Aula 08 - Filtrando uma tabela - Filtrando com expressao regular"
 git push -u -f origin master
--- js/filter.js
+++ js/filter.js
@@ -1,18 +1,19 @@
 filtro.addEventListener("input",function(){
 	var input = this.value;
 	
+	var clientes = document.querySelectorAll(".linha-paciente"); 
 	if(this.value.length>0){
 	
 	for (var i=0; i<clientes.length; i++) {
 	var cliente = clientes[i];
 	var tdnome = cliente.querySelector(".info-nome");
 		var nome = tdnome.textContent;
-		if (nome != this.value){
+		var expressao = new RegExp(this.value,"i");
+		if (!expressao.test(nome)){
 			cliente.classList.add("invisible");
 		}else{
 			cliente.classList.remove("invisible");
 		}
-		//event.target.remove();
 	}
 	}else{
 		for (var i = 0; i< clientes.length; i++){
@@ -20,21 +21,6 @@ filtro.addEventListener("input",function(){
 			cliente.classList.remove("invisible");
 		}
 	}
-	/*clientes.forEach(function(nome){
-			let cliente = clientes.querySelector("info-nome");
-			cliente.textContent = nome;
-	});*/
+
 });
 
-// grid.addEventListener("dblclick",function(event){
-	// event.target.parentNode.classList.add("fadeOut");
-
-	// setTimeout(function(){
-		// event.target.parentNode.remove();
-	// },500);
-// });
-
-
-grid.addEventListener("click",function(event){
-	console.log(event.target.textContent);
-});
