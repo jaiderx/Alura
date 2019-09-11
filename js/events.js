c6ac6294753c733887cd42064797990eb4dd266f
jaiderx <jaiderxavierjunior@gmail.com>
2019-09-11 16:03:58 -0400

    Correcao cores CSS

--- js/events.js
+++ js/events.js
@@ -6,6 +6,28 @@ grid.addEventListener("dblclick",function(event){
 	},500);
 });
 
-// grid.addEventListener("click",function(event){
-	// console.log(event.target.textContent);
-// });
+filtro.addEventListener("input",function(){
+	var input = this.value;
+	
+	var clientes = document.querySelectorAll(".linha-paciente"); 
+	if(this.value.length>0){
+	
+	for (var i=0; i<clientes.length; i++) {
+	var cliente = clientes[i];
+	var tdnome = cliente.querySelector(".info-nome");
+		var nome = tdnome.textContent;
+		var expressao = new RegExp(this.value,"i");
+		if (!expressao.test(nome)){
+			cliente.classList.add("invisible");
+		}else{
+			cliente.classList.remove("invisible");
+		}
+	}
+	}else{
+		for (var i = 0; i< clientes.length; i++){
+			var cliente = clientes[i];
+			cliente.classList.remove("invisible");
+		}
+	}
+
+});
--- js/filter.js
+++ js/filter.js
@@ -1,26 +1,2 @@
-filtro.addEventListener("input",function(){
-	var input = this.value;
-	
-	var clientes = document.querySelectorAll(".linha-paciente"); 
-	if(this.value.length>0){
-	
-	for (var i=0; i<clientes.length; i++) {
-	var cliente = clientes[i];
-	var tdnome = cliente.querySelector(".info-nome");
-		var nome = tdnome.textContent;
-		var expressao = new RegExp(this.value,"i");
-		if (!expressao.test(nome)){
-			cliente.classList.add("invisible");
-		}else{
-			cliente.classList.remove("invisible");
-		}
-	}
-	}else{
-		for (var i = 0; i< clientes.length; i++){
-			var cliente = clientes[i];
-			cliente.classList.remove("invisible");
-		}
-	}
 
-});
 
